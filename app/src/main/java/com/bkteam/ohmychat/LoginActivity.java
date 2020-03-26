25cd6d9994e741716a83833590338da2b2e61d5d
myshine112 <greensatellite777@gmail.com>
2020-03-26 10:43:52 +0700

    initial commit BOOK

---
+++ app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
@@ -0,0 +1,175 @@
+package com.bkteam.ohmychat;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+
+    private Button loginButton, phoneLoginButton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink;
+    private TextView forgotPasswordLink;
+    private ProgressDialog loadingBar;
+
+    private DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        InitializeFields();
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
+
+        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ResetPassword();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
+
+
+    }
+
+    private void ResetPassword() {
+        if (userEmail.getText().toString().matches(""))
+            Toast.makeText(LoginActivity.this, "Please enter the account's email!", Toast.LENGTH_SHORT).show();
+        else{
+            mAuth.sendPasswordResetEmail(userEmail.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                                Toast.makeText(LoginActivity.this, "Failed to reset password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void AllowUserToLogin() {
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_LONG).show();
+        }
+        else{
+            loadingBar.setTitle(R.string.logging_in);
+            loadingBar.setMessage("Please wait....");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String currentUserId = mAuth.getCurrentUser().getUid();
+                                String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                userRef.child(currentUserId).child("deviceToken")
+                                        .setValue(deviceToken)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    SendUserToMainActivity();
+                                                    Toast.makeText(LoginActivity.this, "Logged", Toast.LENGTH_LONG).show();
+                                                    loadingBar.dismiss();
+                                                }
+
+                                            }
+                                        });
+
+
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this,
+                                        "Logged in Successfull...", Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    private void InitializeFields() {
+        loginButton = (Button) findViewById(R.id.login_button);
+        userEmail = (EditText) findViewById(R.id.login_email);
+        userPassword = (EditText) findViewById(R.id.login_password);
+        needNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        forgotPasswordLink = (TextView) findViewById(R.id.forgot_password_link);
+        loadingBar = new ProgressDialog(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser != null) {
+            SendUserToMainActivity();
+        }
+    }
+
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+}
